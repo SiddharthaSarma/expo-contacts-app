71cd1b9d5493aae4b5f16289084b84b917cbad88
siddharthasarma <sidduinevitable@gmail.com>
2019-06-05 22:44:27 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,67 @@
+import { Contacts } from 'expo';
+import React from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Header, ListItem } from 'react-native-elements';
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      contacts: []
+    };
+  }
+
+  loadContacts = async () => {
+    const permission = await Expo.Permissions.askAsync(
+      Expo.Permissions.CONTACTS
+    );
+
+    if (permission.status !== 'granted') {
+      return;
+    }
+
+    const { data } = await Contacts.getContactsAsync({
+      fields: [
+        Contacts.Fields.PhoneNumbers,
+        Contacts.Fields.Emails,
+        Contacts.Fields.Image
+      ]
+    });
+
+    console.log(data);
+    this.setState({ contacts: data, isLoading: false });
+  };
+  componentDidMount() {
+    this.loadContacts();
+  }
+  render() {
+    return (
+      <View>
+        <Header
+          centerComponent={{ text: 'Contacts', style: { color: '#fff' } }}
+        />
+        <ScrollView>
+          <View>
+            {this.state.contacts.map((l, i) => (
+              <ListItem
+                key={i}
+                title={l.firstName + ' ' + l.lastName}
+                subtitle={l.phoneNumbers ? l.phoneNumbers[0].number : ' '}
+                bottomDivider
+              />
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
